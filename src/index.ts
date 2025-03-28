2f41b2304b76064fa1d2eeb434ed5e301766625a
Sam <asamadans@gmail.com>
2025-03-29 00:13:37 +0530

    bridiging with yuzu

---
+++ src/index.ts
@@ -0,0 +1,46 @@
+import { ethers } from "ethers";
+import dotenv from "dotenv";
+import axios from "axios";
+
+dotenv.config();
+
+const wallet = process.env.WALLET || "";
+
+const BASE_URL = "https://yuzu-api-production.r8edev.xyz/bridge/arbMainnet/eduMainnet";
+
+export async function approve(pubkey: String, amount: String) {
+    try {
+        const response = await axios.get(`${BASE_URL}/${pubkey}/approve/edu/${amount}`);
+        console.log(response.data);
+        return response.data; // Returns the EVM transaction object
+    } catch (error) {
+        console.error('Error in approve function:', error);
+        throw error;
+    }
+}
+
+export async function deposit(pubkey: String, amount: String) {
+    try {
+        const response = await axios.get(`${BASE_URL}/${pubkey}/deposit/edu/${amount}`);
+        console.log(response.data);
+        return response.data; // Returns the EVM transaction object
+    } catch (error) {
+        console.error('Error in deposit function:', error);
+        throw error;
+    }
+}
+
+export async function withdraw(pubkey: String, amount: String) {
+    try {
+        const response = await axios.get(`${BASE_URL}/${pubkey}/withdraw/edu/${amount}`);
+        console.log(response.data);
+        return response.data; // Returns the EVM transaction object
+    } catch (error) {
+        console.error('Error in withdraw function:', error);
+        throw error;
+    }
+}
+
+approve(wallet, "69");
+deposit(wallet, "69");
+withdraw(wallet, "69");
